1caf2ee603578c99d4fe9bb20cc3a6504fdd3c8c
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-07-11 14:25:04 +0800

    add routers/controller, error/exception handling, Connected to Database, register User,login User,CREATE JWT
    npm install --save express-async-handler
    npm install jsonwebtoken

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const {registerUser,loginUser} = require('../controllers/userController')
 
 
-router.post('/', (req, res) => {
-    res.send('Register Route')
-})
-
-router.post('/login', (req, res) => {
-    res.send('Login Route')
-})
+router.post('/', registerUser)
+router.post('/login', loginUser)
 
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+//Connect to database
+connectDB()
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
     res.status(200).json({message:'Welcome to the support centre API '})
 })
 
 //Routes
-app.use('/api/users',require('./routes/userRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT,()=>console.log('server running on port',PORT))
