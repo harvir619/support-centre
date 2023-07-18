31ce3284073fb0550b4c61f98647bd5da9a26dd2
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-07-18 16:05:16 +0800

    Authentication jsonwebtoken middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,9 +75,20 @@ const loginUser = asyncHandler(async (req, res) => {
         res.status(401)
         throw new Error('Invalid credentials')
     }
-    
-    
 })
+
+// @desc Get current user
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name:req.user.name,
+    }
+    res.status(200).json(user)
+})
+
 
 // Generate Token
 const generateToken = (id) => {
@@ -88,6 +99,7 @@ const generateToken = (id) => {
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
 
