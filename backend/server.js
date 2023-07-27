a80bfce321912c5650a8b991d6c1ccfee1044486
Harvir Brar <83703941+harvir619@users.noreply.github.com>
2023-07-27 11:15:26 +0800

    retry deploy render

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,10 @@ if (process.env.NODE_ENV === 'production') {
     // Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*',(req,res)=>res.sendFile(__dirname,'../','frontend','build','index.html'))
+    //Any route that is not api will be directed to index.html
+    app.get('*',(req,res)=>
+     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    )
 } else {
     app.get('/', (req, res) => {
     res.status(200).json({message:'Welcome to the support centre API '})
